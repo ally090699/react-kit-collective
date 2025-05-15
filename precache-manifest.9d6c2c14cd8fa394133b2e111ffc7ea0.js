90bcc073f6a85e6a2655f95ccabba92487b702a1
Allison So <aso43@uwo.ca>
2025-05-15 14:48:07 -0400

    Updates

---
+++ precache-manifest.9d6c2c14cd8fa394133b2e111ffc7ea0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac0b1aa68191fb939cd30c12ab5b543",
+    "url": "/react-kit-collective/index.html"
+  },
+  {
+    "revision": "ffe8e718dde16f8a9e3f",
+    "url": "/react-kit-collective/static/css/main.9c17a0ce.chunk.css"
+  },
+  {
+    "revision": "ee54348fd8588ad0e5b9",
+    "url": "/react-kit-collective/static/js/2.1140cd96.chunk.js"
+  },
+  {
+    "revision": "ffe8e718dde16f8a9e3f",
+    "url": "/react-kit-collective/static/js/main.b48147c6.chunk.js"
+  },
+  {
+    "revision": "28fe43d8dfe7306aefcd",
+    "url": "/react-kit-collective/static/js/runtime-main.df04ceaf.js"
+  },
+  {
+    "revision": "ac4e5a7db6fd681bcfeb00c0e24e8426",
+    "url": "/react-kit-collective/static/media/about-1.ac4e5a7d.jpg"
+  },
+  {
+    "revision": "95325c75b384b891dffe9292f2052749",
+    "url": "/react-kit-collective/static/media/about-2.95325c75.jpg"
+  },
+  {
+    "revision": "807b4f4cc0de89fa32cec392f52e2cca",
+    "url": "/react-kit-collective/static/media/aboutteam-1.807b4f4c.jpg"
+  },
+  {
+    "revision": "4e7fb59e973064dcb5a4e58c49d1f220",
+    "url": "/react-kit-collective/static/media/aboutteam-2.4e7fb59e.jpg"
+  },
+  {
+    "revision": "c73edc4329b19a8ffaddbe993cf729a3",
+    "url": "/react-kit-collective/static/media/aboutteam-3.c73edc43.jpg"
+  },
+  {
+    "revision": "7ce0e8b86423b186ace3f0e2004e2995",
+    "url": "/react-kit-collective/static/media/banner.7ce0e8b8.jpg"
+  },
+  {
+    "revision": "e05d4464e6f6f7f4d3cb7de42a7449cf",
+    "url": "/react-kit-collective/static/media/carousel-1.e05d4464.jpg"
+  },
+  {
+    "revision": "2f5f31ee776b81ed88517f93e8dec66c",
+    "url": "/react-kit-collective/static/media/carousel-2.2f5f31ee.jpg"
+  },
+  {
+    "revision": "5b94fe6952113ce1e17f544150dd9f6c",
+    "url": "/react-kit-collective/static/media/carousel-3.5b94fe69.jpg"
+  },
+  {
+    "revision": "11b05697095b7d664264c0aa5c0cb8d6",
+    "url": "/react-kit-collective/static/media/padded.11b05697.png"
+  },
+  {
+    "revision": "96bd43926701e6b415f6f285859e4cf4",
+    "url": "/react-kit-collective/static/media/parallax.96bd4392.jpg"
+  },
+  {
+    "revision": "8c75c4df2e51574056537bb0ae72fd04",
+    "url": "/react-kit-collective/static/media/product-1.8c75c4df.jpg"
+  },
+  {
+    "revision": "b0a4e707f09386c9215050cb365298d5",
+    "url": "/react-kit-collective/static/media/product-2.b0a4e707.jpg"
+  },
+  {
+    "revision": "b3699668029180d7343ed1b6e309a39d",
+    "url": "/react-kit-collective/static/media/product-3.b3699668.jpg"
+  },
+  {
+    "revision": "6603c996ade2da7c298c794f9bca561f",
+    "url": "/react-kit-collective/static/media/product-4.6603c996.jpg"
+  }
+]);
